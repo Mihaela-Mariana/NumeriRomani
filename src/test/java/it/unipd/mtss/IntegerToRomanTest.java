1f784a9eef01fcf0a82ce434dbfb3dee0a9b5e5a
Mario1535 <mariocurzio.depasquale@gmail.com>
2024-05-15 16:20:48 +0200

    creazioni primi test #8

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,5 +1,58 @@
 package it.unipd.mtss;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class IntegerToRomanTest {
-    
+    @Test
+    public void testConvertZero() {
+        // Arrange
+        int numeroArabo = 0;
+        String numeroRomanoAtteso = "";
+
+        // Act
+        String numeroRomanoAttuale = IntegerToRoman.convert(numeroArabo);
+
+        // Assert
+        assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
+    }
+
+    @Test
+    public void testConvert1() {
+        // Arrange
+        int numeroArabo = 1;
+        String numeroRomanoAtteso = "I";
+
+        // Act
+        String numeroRomanoAttuale = IntegerToRoman.convert(numeroArabo);
+
+        // Assert
+        assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
+    }
+
+    @Test
+    public void testConvert2() {
+        // Arrange
+        int numeroArabo = 2;
+        String numeroRomanoAtteso = "II";
+
+        // Act
+        String numeroRomanoAttuale = IntegerToRoman.convert(numeroArabo);
+
+        // Assert
+        assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
+    }
+
+    @Test
+    public void testConvert3() {
+        // Arrange
+        int numeroArabo = 3;
+        String numeroRomanoAtteso = "III";
+
+        // Act
+        String numeroRomanoAttuale = IntegerToRoman.convert(numeroArabo);
+
+        // Assert
+        assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
+    }
 }
